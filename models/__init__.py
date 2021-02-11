bbea2667ae0186637840efb8b0d3f61f5c8e1018
veredeps <veredeps@gmail.com>
2021-02-11 11:09:00 +0200

    build redetector

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,15 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .detr import build
-
+from .redetector import build as build_redetector
+from .redetector import build_from_detr
 
 def build_model(args):
     return build(args)
+
+
+def build_redetectr_model(args):
+    return build_redetector(args)
+
+
+def build_redetector_from_detr(args, detr_model, criterion):
+    return build_from_detr(args, detr_model, criterion)
